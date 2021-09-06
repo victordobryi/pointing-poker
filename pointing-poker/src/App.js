d55a7b912b34a16cfb21349fc311e1c27c9c28f7
Alexej1900 <alexej1900@mail.ru>
2021-09-06 13:28:05 +0300

    init: start settings-form

---
+++ pointing-poker/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import SettingsForm from './components/settings-form/SettingsForm';
+
+function App() {
+  return (
+    <SettingsForm/>
+  );
+}
+
+export default App;
---
+++ pointing-poker/src/components/settings-form/SettingsForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SettingsForm = () => {
+  return (
+    <>
+      <Heading as="h5" size="sm">
+        Game settings:
+      </Heading>
+    </>
+  );
+}
+
+export default SettingsForm;
