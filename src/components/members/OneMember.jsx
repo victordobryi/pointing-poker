437ccdb09abb197bacfad9c532b444ef6027b907
bazla <dobr1nya@mail.ru>
2021-09-08 17:58:28 +0300

    refactor: fix oneMember component

--- src/components/members/OneMember.jsx
+++ src/components/members/OneMember.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box, Flex, Spacer, CloseIcon, Image } from '@chakra-ui/react';
-
+import deleteIcon from '../../assets/icons/delete.svg';
 const OneMember = ({ position, name, image, deleteClick }) => {
   return (
     <div>
@@ -14,20 +14,25 @@ const OneMember = ({ position, name, image, deleteClick }) => {
         _hover={{ cursor: 'pointer' }}
       >
         <Flex align="center" justify="center">
-          <Box fontSize={20} fontWeight="bold" h="20px">
-            {name}
-          </Box>
-          <Spacer />
-          <Box fontSize={10} fontWeight="bold" h="20px">
-            {position}
-          </Box>
-          <Spacer />
           <Image
             src={image}
             alt="Card-image"
             boxSize="60px"
             objectFit="cover"
           />
+          <Spacer />
+
+          <Flex flexDirection={'column'} h={50} justifyContent="space-between">
+            <Box fontSize={20} fontWeight="bold" h="20px">
+              {name}
+            </Box>
+            <Box fontSize={10} fontWeight="bold" h="20px">
+              {position}
+            </Box>
+          </Flex>
+
+          <Spacer />
+          <Image src={deleteIcon} w={30} />
           {/* <CloseIcon w="20px" h="20px" color="red" onClick={deleteClick} /> */}
         </Flex>
       </Box>
