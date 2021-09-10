e2e0bd75cb9760c172c02be90e56d144bea9dcee
bazla <dobr1nya@mail.ru>
2021-09-10 13:31:38 +0300

    feat: add mainLayout

--- pointing-poker/src/pages/result-page/result-page.jsx
+++ pointing-poker/src/pages/result-page/result-page.jsx
@@ -1,5 +1,21 @@
-import { Fragment } from 'react';
-
+import { MainLayout } from '../../components/mainLayout/mainLayout';
+import { Flex, Img } from '@chakra-ui/react';
+import pencil from '../../assets/icons/pencil.png';
+const issuesNumbers = [13, 19, 322, 533, 666, 245, 900, 400, 3232, 455656];
 export const ResultPage = () => {
-  return <Fragment>Hello</Fragment>;
+  return (
+    <MainLayout>
+      <Flex justifyContent={'center'}>
+        Spring 23 planning (issues {''}
+        {issuesNumbers.map((issue, index) =>
+          issuesNumbers.length > index + 1 ? (
+            <span key={index}>{issue}, </span>
+          ) : (
+            <span key={index}>{issue} </span>
+          )
+        )}
+        )<Img src={pencil}></Img>
+      </Flex>
+    </MainLayout>
+  );
 };
