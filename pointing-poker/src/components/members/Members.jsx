6e737d669287fbec5d9f3a7b9160d2931945b0e9
Alexej Bodnsrchuk <alexej1900@mail.ru>
2021-09-24 15:34:23 +0300

    add: deleting member with voting

--- pointing-poker/src/components/members/Members.jsx
+++ pointing-poker/src/components/members/Members.jsx
@@ -4,24 +4,41 @@ import OneMember from './OneMember';
 import NoMembersCard from './NoMembersCard';
 import { Modal } from '../modal/modal';
 import { KickPlayerModal } from '../modals/KickPlayerModal';
+import { DeletePlayerModal } from '../modals/DeletePlayerModal';
 import { UsersContext } from '../../contexts/usersContext';
+import { SocketContext } from '../../contexts/socketContext';
 
 const Members = () => {
   const [modalActive, setModalActive] = useState(false);
-  const [deletedMember, setDeletedMember] = useState('');
+  const [deletedUser, setDeletedUser] = useState('');
+  const [kicker, setKicker] = useState('');
+  const [voteSet, setVoteSet] = useState('');
   const { users } = useContext(UsersContext);
+  const socket = useContext(SocketContext);
 
   let arrayMembers = users.filter((user) => user.isMaster === false);
 
-  const handleDelClick = (id) => {
-    setDeletedMember(id);
+  socket.on('willPlayerKick', ({deletedUser, kicker, voteSet}) => {
+    setDeletedUser(deletedUser);
+    setKicker(kicker);
+    setVoteSet(voteSet);
     setModalActive(true);
+  });
+
+  const handleDelClick = (idd, openModal) => {
+    setDeletedUser(getDeletedUser(idd));
+    setKicker('');
+    openModal && setModalActive(true);
+  }
+
+  const getDeletedUser = (idd) => {
+    return (users.filter((user) => user.idd === idd))[0];
   }
 
   const getMemberName = () => {
     let memberName = '';
     arrayMembers.forEach((member) => {
-      if (member.idd === deletedMember) {
+      if (member.idd === deletedUser.idd) {
         memberName = member.fullName;
       }
     });
@@ -48,12 +65,20 @@ const Members = () => {
           )}
         </Flex>
       </Box>
-      <Modal active={modalActive} setActive={setModalActive}>
-        <KickPlayerModal
-          id={deletedMember}
-          memberName={getMemberName()}
-          onClose={setModalActive}
-        />
+      <Modal active={modalActive} setActive={setModalActive} closeOnOverlayClick={false} >
+        {kicker !== ''
+          ? <DeletePlayerModal 
+              kicker={kicker} 
+              deletedUser={deletedUser} 
+              voteSet={voteSet}
+              onClose={setModalActive}
+            />
+          : <KickPlayerModal
+              id={deletedUser.idd}
+              memberName={getMemberName()}
+              onClose={setModalActive}
+            />
+          }
       </Modal>
     </>
   );
