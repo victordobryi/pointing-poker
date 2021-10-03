9b3209be3cd8cc71810f1ad661c5573005cddc65
barmenski <1kiloom@gmail.com>
2021-10-03 20:34:22 +0300

    fix: some conflict merge

--- pointing-poker/src/components/mainLayout/mainLayout.jsx
+++ pointing-poker/src/components/mainLayout/mainLayout.jsx
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import { Fragment } from 'react';
-import logo from '../../assets/icons/logo.png';
-import logoRss from '../../assets/icons/logo-rs.svg';
-import styles from './mainLayout.module.scss';
-import { Img } from '@chakra-ui/image';
-import { Link } from '@chakra-ui/layout';
-export const MainLayout = ({ children }) => {
-=======
 import React, { useState, Fragment, useEffect, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { SocketContext } from '../../contexts/socketContext';
@@ -52,30 +43,12 @@ export const MainLayout = ({ children }) => {
     history.push('/');
   };
 
->>>>>>> develop
   return (
     <Fragment>
       <div className={styles.header__firstRow}></div>
       <div className={styles.header__secondRow}></div>
       <div className={styles.wrapper}>
         <div className={styles.logo}>
-<<<<<<< HEAD
-          <Img src={logo} alt="logo" width={70} height={70} />
-        </div>
-        <main>{children}</main>
-      </div>
-      <footer className={styles.footer}>
-        <div className={styles.wrapper}>
-          <div className={styles.footer__block}>
-            <Link href="https://github.com/alexej1900">Alexej Bodnarchuk</Link>
-            <Link href="https://github.com/victordobryi">Victar Kasilkin</Link>
-            <Link href="https://github.com/barmenski">Alexandr Bondar</Link>
-            <span>2021</span>
-            <Link href="https://rs.school/react/">
-              <Img src={logoRss} alt="logoRss" width={70} height={70} />
-            </Link>
-          </div>
-=======
           <Img
             src={logo}
             alt='logo'
@@ -126,7 +99,6 @@ export const MainLayout = ({ children }) => {
           <Link href='https://rs.school/react/'>
             <Img src={logoRss} alt='logoRss' width={70} height={70} />
           </Link>
->>>>>>> develop
         </div>
       </footer>
     </Fragment>
--- pointing-poker/src/pages/result-page/result-page.jsx
+++ pointing-poker/src/pages/result-page/result-page.jsx
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-import { MainLayout } from '../../components/mainLayout/mainLayout';
-import { Flex, Img, Box, Spacer } from '@chakra-ui/react';
-import pencil from '../../assets/icons/pencil.png';
-const issuesNumbers = [13, 19, 322, 533, 666, 245, 900, 400, 3232, 455656];
-export const ResultPage = () => {
-  return (
-    <MainLayout>
-      <Flex justifyContent={'center'}>
-        Spring 23 planning (issues {''}
-        {issuesNumbers.map((issue, index) =>
-          issuesNumbers.length > index + 1 ? (
-            <span key={index}>{issue}, </span>
-          ) : (
-            <span key={index}>{issue} </span>
-          )
-        )}
-        )<Img src={pencil}></Img>
-      </Flex>
-      <Flex direction="column" justify="space-around" h="100%">
-        <Flex h="350px" direction="column" justify="space-around">
-          <Box w="250px" h="63" backgroundColor="black"></Box>
-          <Flex justify="space-between" w="50%">
-            <Box h="226" w="140px" backgroundColor="black"></Box>
-            <Box h="226" w="140px" backgroundColor="black"></Box>
-            <Box h="226" w="140px" backgroundColor="black"></Box>
-          </Flex>
-        </Flex>
-        <Flex h="350px" direction="column" justify="space-around">
-          <Box w="250px" h="63" backgroundColor="black"></Box>
-          <Flex justify="space-between" w="50%">
-            <Box h="226" w="140px" backgroundColor="black"></Box>
-            <Box h="226" w="140px" backgroundColor="black"></Box>
-            <Box h="226" w="140px" backgroundColor="black"></Box>
-          </Flex>
-        </Flex>
-      </Flex>
-=======
 import React, { useState, useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { MainLayout } from '../../components/mainLayout/mainLayout';
@@ -133,7 +95,6 @@ export const ResultPage = () => {
       <Modal active={modalActive} setActive={setModalActive}>
         <YouAreDeletedModal isSession={isSession} />
       </Modal>
->>>>>>> develop
     </MainLayout>
   );
 };
