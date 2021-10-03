252c71a8e88c1b02f73a85342272a6778d43c87c
barmenski <1kiloom@gmail.com>
2021-10-04 00:21:45 +0300

    feat: push to heroku

--- pointing-poker/src/contexts/socketContext.js
+++ pointing-poker/src/contexts/socketContext.js
@@ -4,8 +4,8 @@ import io from 'socket.io-client';
 const SocketContext = React.createContext();
 
 const SocketProvider = ({ children }) => {
-  // const ENDPOINT = 'https://socket-chat-ak.herokuapp.com/';
-  const ENDPOINT = 'http://localhost:5000/';
+  // const ENDPOINT = 'http://localhost:5000/';
+  const ENDPOINT = 'https://team25.herokuapp.com/';
   const socket = io(ENDPOINT, { transports: ['websocket', 'polling'] });
 
   return (
