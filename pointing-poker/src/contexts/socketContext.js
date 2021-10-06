61eaee2fade151b585682e578702cddd374ddd75
barmenski <1kiloom@gmail.com>
2021-10-06 03:12:38 +0300

    feat: change server

--- pointing-poker/src/contexts/socketContext.js
+++ pointing-poker/src/contexts/socketContext.js
@@ -4,10 +4,8 @@ import io from 'socket.io-client';
 const SocketContext = React.createContext();
 
 const SocketProvider = ({ children }) => {
-  const ENDPOINT = 'https://team25.herokuapp.com/';
-  const socket = io(ENDPOINT, {
-    transports: ['websocket', 'polling'],
-  });
+  const ENDPOINT = 'https://team25.eu.openode.io/';
+  const socket = io(ENDPOINT, { transports: ['websocket', 'polling'] });
 
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
